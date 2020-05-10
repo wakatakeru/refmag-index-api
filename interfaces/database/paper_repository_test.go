e7235ed4be2c405dac800366de5432575fa66781
wakatakeru <s19g460@stu.kagawa-u.ac.jp>
2020-05-10 21:02:30 +0900

    add test FindByID()

--- interfaces/database/paper_repository_test.go
+++ interfaces/database/paper_repository_test.go
@@ -10,13 +10,13 @@ import (
 func TestStore(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
+	mockSqlHandler := NewMockSqlHandler(ctrl)
+	mockSqlResult := NewMockSqlResult(ctrl)
+
 	paper := domain.Paper{}
 	query := "INSERT INTO papers (title, doi, supplement) VALUES (?,?,?)"
 	var expectedID int64
 	var err error
-
-	mockSqlHandler := NewMockSqlHandler(ctrl)
-	mockSqlResult := NewMockSqlResult(ctrl)
 
 	mockSqlHandler.EXPECT().Execute(query, paper.Title, paper.DOI, paper.Supplement).Return(mockSqlResult, err)
 	mockSqlResult.EXPECT().LastInsertId().Return(expectedID, err)
@@ -30,7 +30,30 @@ func TestStore(t *testing.T) {
 }
 
 func TestFindByID(t *testing.T) {
-	return
+	ctrl := gomock.NewController(t)
+
+	mockSqlHandler := NewMockSqlHandler(ctrl)
+	mockRow := NewMockRow(ctrl)
+
+	query := "SELECT id, title, doi, supplement FROM papers WHERE id = ?"
+	var paperID int
+	var err error
+	var id int
+	var title string
+	var doi string
+	var supplement string
+
+	mockSqlHandler.EXPECT().Query(query, paperID).Return(mockRow, err)
+	mockRow.EXPECT().Next()
+	mockRow.EXPECT().Scan(&id, &title, &doi, &supplement).Return(err)
+	mockRow.EXPECT().Close()
+
+	paperRepository := NewPaperRepository(mockSqlHandler)
+	_, err = paperRepository.FindByID(paperID)
+
+	if err != nil {
+		t.Error("FindByID is not same as expected")
+	}
 }
 
 func TestFindAll(t *testing.T) {
